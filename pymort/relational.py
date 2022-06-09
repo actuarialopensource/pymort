8bdfec8cb80ab71100ae5d27109ae4bdf64ae20f
Matthew Caseres <matthewcaseres@outlook.com>
2022-06-08 21:01:08 -0500

    Retrieve groupings easily from relational table
    This should make it easier to get all the tables for a particular grouping.

--- pymort/relational.py
+++ pymort/relational.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
 import importlib.resources
 from . import pickled_tables
 import pandas as pd
 import pickle
+from typing import List
+from itertools import groupby
 
 class Relational:
     """Rate tables with primary/foreign keys.
@@ -20,4 +23,29 @@ class Relational:
         self.metadata: pd.DataFrame = pickle.loads(importlib.resources.read_binary(pickled_tables, 'meta.pickle'))
         self.select: pd.DataFrame = pickle.loads(importlib.resources.read_binary(pickled_tables, 'sel.pickle'))
         self.ultimate: pd.DataFrame = pickle.loads(importlib.resources.read_binary(pickled_tables, 'ult.pickle'))
+
+@dataclass
+class IdGroup:
+    """
+    An IdGroup is a group of IDs that have the same study and grouping in the Relational().metadata pandas table
+    """
+    study: str
+    grouping: str
+    ids: List[int]
+
+
+def getIdGroup(targetId: int) -> IdGroup:
+    """
+    Returns an object representing the group of IDs that have the same study and grouping in the Relational().metadata pandas table.
+    """
+    related = Relational()
+    meta = related.metadata
+    meta.reset_index(inplace=True)
+    # ensure that study/grouping groups are all consecutive
+    meta.sort_values(by=["study", "grouping", "id"], inplace=True)
+    for k, g in groupby(zip(meta.id, meta.study, meta.grouping), key=lambda x: (x[1], x[2])):
+        groupIds = [x[0] for x in g]
+        if targetId in groupIds:
+            return IdGroup(k[0], k[1], groupIds)
+    raise KeyError("Your table identifier is not in the pandas table Relational().metadata")
 
