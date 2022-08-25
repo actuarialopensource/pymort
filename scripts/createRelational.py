fb228a8c597a3cfc9a82c93c25490e3f0b316a61
Matthew Caseres <matthewcaseres@outlook.com>
2022-08-25 12:55:28 -0500

    black fmt

--- scripts/createRelational.py
+++ scripts/createRelational.py
@@ -4,40 +4,43 @@ from pymort import PyXML
 from pathlib import Path
 import pickle
 
+
 def getSelectUltimate(metaDF: pd.DataFrame) -> pd.DataFrame:
-    '''
+    """
     Given a pandas DataFrame of MetaData, return the values metadata
-    '''
+    """
     selects = []
     ults = []
     for x in metaDF.index.values:
         xtbml = PyXML(x)
         select = xtbml.Tables[0].Values
-        select['id'] = x
+        select["id"] = x
         selects.append(select)
 
         ult = xtbml.Tables[1].Values
-        ult['id'] = x
+        ult["id"] = x
         ults.append(ult)
-    
+
     ultsDF = pd.concat(ults)
     ultsDF.reset_index(inplace=True)
-    ultsDF.set_index(['id', 'Age'], inplace=True)
+    ultsDF.set_index(["id", "Age"], inplace=True)
     ultsDF.sort_index(inplace=True)
 
     selDF = pd.concat(selects)
     selDF.reset_index(inplace=True)
-    selDF.set_index(['id', 'Age', 'Duration'], inplace=True)
+    selDF.set_index(["id", "Age", "Duration"], inplace=True)
     selDF.sort_index(inplace=True)
-    
+
     return selDF, ultsDF
+
 
 def getMeta() -> pd.DataFrame:
     df = pd.concat([get2015VBT(), get2017CSO()])
-    df = df[['id', 'study', 'grouping', 'gender', 'risk']]
-    df.set_index('id', inplace=True)
+    df = df[["id", "study", "grouping", "gender", "risk"]]
+    df.set_index("id", inplace=True)
     df.sort_index(inplace=True)
     return df
+
 
 # There is no easy way to automate this
 def get2017CSO() -> pd.DataFrame:
@@ -47,64 +50,92 @@ def get2017CSO() -> pd.DataFrame:
     age_basis = []
     ids = []
     # LOADED COMPOSITE GENDER BLENDED
-    grouping.extend(['loaded composite gender_blended']*10)
-    risk.extend(["composite"]*10)
+    grouping.extend(["loaded composite gender_blended"] * 10)
+    risk.extend(["composite"] * 10)
     gender.extend([f"{percent}%_male" for percent in [20, 40, 50, 60, 80]] * 2)
-    age_basis.extend(['ANB']*5 + ['ALB']*5)
+    age_basis.extend(["ANB"] * 5 + ["ALB"] * 5)
     ids.extend(list(range(3277, 3287)))
     # LOADED COMPOSITE GENDER DISTINCT
-    grouping.extend(['loaded composite gender_distinct']*4)
-    risk.extend(["composite"]*4)
-    gender.extend(['male', 'female']*2)
-    age_basis.extend(['ANB']*2 + ['ALB']*2)
+    grouping.extend(["loaded composite gender_distinct"] * 4)
+    risk.extend(["composite"] * 4)
+    gender.extend(["male", "female"] * 2)
+    age_basis.extend(["ANB"] * 2 + ["ALB"] * 2)
     ids.extend(range(3287, 3291))
     # LOADED SMOKER AND GENDER DISTINCT
-    grouping.extend(['loaded smoker_distinct gender_distinct']*8)
-    risk.extend((['nonsmoker']*2 + ['smoker']*2)*2)
-    gender.extend(['male', 'female']*4)
-    age_basis.extend(['ANB']*4 + ['ALB']*4)
+    grouping.extend(["loaded smoker_distinct gender_distinct"] * 8)
+    risk.extend((["nonsmoker"] * 2 + ["smoker"] * 2) * 2)
+    gender.extend(["male", "female"] * 4)
+    age_basis.extend(["ANB"] * 4 + ["ALB"] * 4)
     ids.extend(range(3291, 3299))
     # LOADED PREFERRED STRUCTURE
-    grouping.extend(['loaded preferred_structure gender_distinct']*20)
-    risk.extend((['nonsmoker super_preferred', 'nonsmoker preferred', 'nonsmoker residual']*2 + ['smoker preferred', 'smoker residual']*2)*2)
-    gender.extend(((['male']*3 + ['female']*3) + (['male']*2 + ['female']*2))*2)
-    age_basis.extend(['ANB']*10 + ['ALB']*10)
+    grouping.extend(["loaded preferred_structure gender_distinct"] * 20)
+    risk.extend(
+        (
+            ["nonsmoker super_preferred", "nonsmoker preferred", "nonsmoker residual"]
+            * 2
+            + ["smoker preferred", "smoker residual"] * 2
+        )
+        * 2
+    )
+    gender.extend(
+        ((["male"] * 3 + ["female"] * 3) + (["male"] * 2 + ["female"] * 2)) * 2
+    )
+    age_basis.extend(["ANB"] * 10 + ["ALB"] * 10)
     ids.extend(range(3299, 3319))
     # LOADED GENDER BLENDED
-    pGrouping, pSmoke, pBasis, pGender = list(zip(*product(['loaded smoker_distinct gender_blended'], ['nonsmoker', 'smoker'], ['ANB', 'ALB'], [f'{percent}%_male' for percent in [20, 40, 50, 60, 80]])))
+    pGrouping, pSmoke, pBasis, pGender = list(
+        zip(
+            *product(
+                ["loaded smoker_distinct gender_blended"],
+                ["nonsmoker", "smoker"],
+                ["ANB", "ALB"],
+                [f"{percent}%_male" for percent in [20, 40, 50, 60, 80]],
+            )
+        )
+    )
     grouping.extend(pGrouping)
     risk.extend(pSmoke)
     gender.extend(pGender)
     age_basis.extend(pBasis)
     ids.extend(range(3319, 3339))
     # UNLOADED PREFERRED STRUCTURE
-    grouping.extend(['unloaded preferred_structure gender_distinct']*20)
-    risk.extend((['nonsmoker super_preferred', 'nonsmoker preferred', 'nonsmoker residual']*2 + ['smoker preferred', 'smoker residual']*2)*2)
-    gender.extend(((['male']*3 + ['female']*3) + (['male']*2 + ['female']*2))*2)
-    age_basis.extend(['ANB']*10 + ['ALB']*10)
+    grouping.extend(["unloaded preferred_structure gender_distinct"] * 20)
+    risk.extend(
+        (
+            ["nonsmoker super_preferred", "nonsmoker preferred", "nonsmoker residual"]
+            * 2
+            + ["smoker preferred", "smoker residual"] * 2
+        )
+        * 2
+    )
+    gender.extend(
+        ((["male"] * 3 + ["female"] * 3) + (["male"] * 2 + ["female"] * 2)) * 2
+    )
+    age_basis.extend(["ANB"] * 10 + ["ALB"] * 10)
     ids.extend(range(3341, 3361))
     # UNLOADED COMPOSITE GENDER DISTINCT
-    grouping.extend(['unloaded composite gender_distinct']*4)
-    risk.extend(["composite"]*4)
-    gender.extend(['male', 'female']*2)
-    age_basis.extend(['ANB']*2 + ['ALB']*2)
+    grouping.extend(["unloaded composite gender_distinct"] * 4)
+    risk.extend(["composite"] * 4)
+    gender.extend(["male", "female"] * 2)
+    age_basis.extend(["ANB"] * 2 + ["ALB"] * 2)
     ids.extend(range(3361, 3365))
     # UNLOADED SMOKER AND GENDER DISTINCT
-    grouping.extend(['unloaded smoker_distinct gender_distinct']*8)
-    risk.extend((['nonsmoker']*2 + ['smoker']*2)*2)
-    gender.extend(['male', 'female']*4)
-    age_basis.extend(['ANB']*4 + ['ALB']*4)
+    grouping.extend(["unloaded smoker_distinct gender_distinct"] * 8)
+    risk.extend((["nonsmoker"] * 2 + ["smoker"] * 2) * 2)
+    gender.extend(["male", "female"] * 4)
+    age_basis.extend(["ANB"] * 4 + ["ALB"] * 4)
     ids.extend(range(3365, 3373))
 
     # Put the age basis as part of the grouping. Not all tables have an age basis so it shouldn't be a column.
     grouping = [" ".join(group_basis) for group_basis in zip(grouping, age_basis)]
 
-    cso2017 = {'grouping': grouping, 'risk': risk, 'gender': gender, 'id': ids}
-    
+    cso2017 = {"grouping": grouping, "risk": risk, "gender": gender, "id": ids}
+
     # load the dict as a pandas dataframe
     df2017 = pd.DataFrame(cso2017)
-    df2017['study'] = "2017_CSO"
+    df2017["study"] = "2017_CSO"
     return df2017
+
 
 def get2015VBT() -> pd.DataFrame:
     risk = []
@@ -113,61 +144,82 @@ def get2015VBT() -> pd.DataFrame:
     age_basis = []
     ids = []
 
-    pGender, pBasis, pRisk =zip(*product(['female'], ['ALB', 'ANB'], [f"nonsmoker RR{x}" for x in [50, 60, 70, 80, 90, 100, 110, 125, 150, 175]]))
+    pGender, pBasis, pRisk = zip(
+        *product(
+            ["female"],
+            ["ALB", "ANB"],
+            [f"nonsmoker RR{x}" for x in [50, 60, 70, 80, 90, 100, 110, 125, 150, 175]],
+        )
+    )
     gender.extend(pGender)
-    grouping.extend(['relative_risk']*20)
+    grouping.extend(["relative_risk"] * 20)
     risk.extend(pRisk)
     age_basis.extend(pBasis)
     ids.extend(range(3209, 3229))
 
-    pGender, pBasis, pRisk =zip(*product(['female'], ['ALB', 'ANB'], [f"smoker RR{x}" for x in [75, 100, 125, 150]]))
+    pGender, pBasis, pRisk = zip(
+        *product(
+            ["female"], ["ALB", "ANB"], [f"smoker RR{x}" for x in [75, 100, 125, 150]]
+        )
+    )
     gender.extend(pGender)
-    grouping.extend(['relative_risk']*8)
+    grouping.extend(["relative_risk"] * 8)
     risk.extend(pRisk)
     age_basis.extend(pBasis)
     ids.extend(range(3229, 3237))
 
-    pGender, pBasis, pRisk =zip(*product(['male'], ['ALB', 'ANB'], [f"nonsmoker RR{x}" for x in [50, 60, 70, 80, 90, 100, 110, 125, 150, 175]]))
+    pGender, pBasis, pRisk = zip(
+        *product(
+            ["male"],
+            ["ALB", "ANB"],
+            [f"nonsmoker RR{x}" for x in [50, 60, 70, 80, 90, 100, 110, 125, 150, 175]],
+        )
+    )
     gender.extend(pGender)
-    grouping.extend(['relative_risk']*20)
+    grouping.extend(["relative_risk"] * 20)
     risk.extend(pRisk)
     age_basis.extend(pBasis)
     ids.extend(range(3237, 3257))
 
-    pGender, pBasis, pRisk =zip(*product(['male'], ['ALB', 'ANB'], [f"smoker RR{x}" for x in [75, 100, 125, 150]]))
+    pGender, pBasis, pRisk = zip(
+        *product(
+            ["male"], ["ALB", "ANB"], [f"smoker RR{x}" for x in [75, 100, 125, 150]]
+        )
+    )
     gender.extend(pGender)
-    grouping.extend(['relative_risk']*8)
+    grouping.extend(["relative_risk"] * 8)
     risk.extend(pRisk)
     age_basis.extend(pBasis)
     ids.extend(range(3257, 3265))
 
-    gender.extend(['male', 'female']*4)
-    grouping.extend(['smoker_distinct']*8)
-    risk.extend((['nonsmoker']*2 + ['smoker']*2)*2)
-    age_basis.extend(['ANB']*4 + ['ALB']*4)
+    gender.extend(["male", "female"] * 4)
+    grouping.extend(["smoker_distinct"] * 8)
+    risk.extend((["nonsmoker"] * 2 + ["smoker"] * 2) * 2)
+    age_basis.extend(["ANB"] * 4 + ["ALB"] * 4)
     ids.extend(range(3265, 3273))
 
-    gender.extend(['male', 'female']*2)
-    grouping.extend(['unismoke']*4)
-    risk.extend(['unismoke']*4)
-    age_basis.extend(['ANB']*2 + ['ALB']*2)
+    gender.extend(["male", "female"] * 2)
+    grouping.extend(["unismoke"] * 4)
+    risk.extend(["unismoke"] * 4)
+    age_basis.extend(["ANB"] * 2 + ["ALB"] * 2)
     ids.extend(range(3273, 3277))
 
     # put the age_basis as part of the grouping
     grouping = [" ".join(group_basis) for group_basis in zip(grouping, age_basis)]
 
     # load the dict as a pandas dataframe
-    vbt2015 = {'risk': risk, 'grouping': grouping, 'gender': gender, 'id': ids}
+    vbt2015 = {"risk": risk, "grouping": grouping, "gender": gender, "id": ids}
     df2015 = pd.DataFrame(vbt2015)
-    df2015['study'] = '2015_VBT'
+    df2015["study"] = "2015_VBT"
     return df2015
+
 
 if __name__ == "__main__":
     meta = getMeta()
     sel, ult = getSelectUltimate(meta)
-    with open(Path('pymort', 'pickled_tables', 'meta.pickle'), 'wb') as f:
+    with open(Path("pymort", "pickled_tables", "meta.pickle"), "wb") as f:
         pickle.dump(meta, f)
-    with open(Path('pymort', 'pickled_tables', 'sel.pickle'), 'wb') as f:
+    with open(Path("pymort", "pickled_tables", "sel.pickle"), "wb") as f:
         pickle.dump(sel, f)
-    with open(Path('pymort', 'pickled_tables', 'ult.pickle'), 'wb') as f:
+    with open(Path("pymort", "pickled_tables", "ult.pickle"), "wb") as f:
         pickle.dump(ult, f)
--- tests/test_XML.py
+++ tests/test_XML.py
@@ -1,26 +1,49 @@
 from pymort import MortXML
 
+
 def test_PyXML():
     xml = MortXML(3279)
 
     contentClassification = xml.ContentClassification
     assert contentClassification.TableIdentity == 3279
     assert contentClassification.ProviderDomain == "soa.org"
-    assert contentClassification.ProviderName == "American Academy of Actuaries along with the Society of Actuaries"
-    assert contentClassification.TableReference == """Joint American Academy of Actuaries' Life Experience Committee and Society of Actuaries Preferred Mortality Oversight Group CSO Development Subgroup, "Report on the 2017 CSO  and on the 2017 CSO Preferred Structure Table Development", American Academy of Actuaries, (2015). Appendix K. Accessed: February, 2016 from https://www.soa.org/resources/experience-studies/2015/2017-cso-tables/"""
+    assert (
+        contentClassification.ProviderName
+        == "American Academy of Actuaries along with the Society of Actuaries"
+    )
+    assert (
+        contentClassification.TableReference
+        == """Joint American Academy of Actuaries' Life Experience Committee and Society of Actuaries Preferred Mortality Oversight Group CSO Development Subgroup, "Report on the 2017 CSO  and on the 2017 CSO Preferred Structure Table Development", American Academy of Actuaries, (2015). Appendix K. Accessed: February, 2016 from https://www.soa.org/resources/experience-studies/2015/2017-cso-tables/"""
+    )
     assert contentClassification.ContentType == "CSO / CET"
-    assert contentClassification.TableName == "2017 Loaded CSO Composite Gender-Blended 50% Male ANB"
-    assert contentClassification.TableDescription == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Age Nearest Birthday. Minimum Age: 0. Maximum Age: 95."
-    assert contentClassification.Comments == "The 2017 Commissioners Standard Ordinary Tables (CSO) are a series of mortality tables developed for regulatory uses including CRVM, net premium reserves and non-forfeiture determination. The tables were based on life insurance mortality experience from 2002 to 2009, projected with improvement to 2017.  The tables include a margin to cover the variation of individual company’s mortality around the industry mean. Table uploaded: 02/2016. Select rates at ages 0-17 for certain durations and ultimate rates at ages 18-27, 29, 30, 32 and 37 were modified 09/2016. Data verified: 07/2018."
-    assert contentClassification.KeyWords == ["Select", "CSO/CET", "United States of America"]
+    assert (
+        contentClassification.TableName
+        == "2017 Loaded CSO Composite Gender-Blended 50% Male ANB"
+    )
+    assert (
+        contentClassification.TableDescription
+        == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Age Nearest Birthday. Minimum Age: 0. Maximum Age: 95."
+    )
+    assert (
+        contentClassification.Comments
+        == "The 2017 Commissioners Standard Ordinary Tables (CSO) are a series of mortality tables developed for regulatory uses including CRVM, net premium reserves and non-forfeiture determination. The tables were based on life insurance mortality experience from 2002 to 2009, projected with improvement to 2017.  The tables include a margin to cover the variation of individual company’s mortality around the industry mean. Table uploaded: 02/2016. Select rates at ages 0-17 for certain durations and ultimate rates at ages 18-27, 29, 30, 32 and 37 were modified 09/2016. Data verified: 07/2018."
+    )
+    assert contentClassification.KeyWords == [
+        "Select",
+        "CSO/CET",
+        "United States of America",
+    ]
 
     # Tests for the select table
     meta0 = xml.Tables[0].MetaData
-    assert meta0.ScalingFactor == 0.
+    assert meta0.ScalingFactor == 0.0
     assert meta0.DataType == "Floating Point"
     assert meta0.Nation == "United States of America"
-    assert meta0.TableDescription == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Select and Ultimate Table. Basis: Age Nearest Birthday. Minimum Select Age: 0. Maximum Select Age: 95."
-    
+    assert (
+        meta0.TableDescription
+        == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Select and Ultimate Table. Basis: Age Nearest Birthday. Minimum Select Age: 0. Maximum Select Age: 95."
+    )
+
     assert meta0.AxisDefs[0].ScaleType == "Age"
     assert meta0.AxisDefs[0].AxisName == "Age"
     assert meta0.AxisDefs[0].MinScaleValue == 0
@@ -34,22 +57,25 @@ def test_PyXML():
     assert meta0.AxisDefs[1].Increment == 1
 
     values0 = xml.Tables[0].Values
-    assert .00029 == list(values0.loc[0, 1])[0]
-    assert .00072 == list(values0.loc[5, 23])[0]
+    assert 0.00029 == list(values0.loc[0, 1])[0]
+    assert 0.00072 == list(values0.loc[5, 23])[0]
 
     # Tests for the ultimate table
     meta1 = xml.Tables[1].MetaData
-    assert meta1.ScalingFactor == 0.
+    assert meta1.ScalingFactor == 0.0
     assert meta1.DataType == "Floating Point"
     assert meta1.Nation == "United States of America"
-    assert meta1.TableDescription == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Select and Ultimate Table. Basis: Age Nearest Birthday. Minimum Ultimate Age: 0. Maximum Ultimate Age: 120"
+    assert (
+        meta1.TableDescription
+        == "2017 Loaded CSO Composite Gender-Blended, Male, 50%, Select and Ultimate Table. Basis: Age Nearest Birthday. Minimum Ultimate Age: 0. Maximum Ultimate Age: 120"
+    )
 
     assert meta1.AxisDefs[0].ScaleType == "Age"
     assert meta1.AxisDefs[0].AxisName == "Age"
     assert meta1.AxisDefs[0].MinScaleValue == 0
     assert meta1.AxisDefs[0].MaxScaleValue == 120
     assert meta1.AxisDefs[0].Increment == 1
-    
+
     values1 = xml.Tables[1].Values
-    assert .00029 == values1.loc[0]["vals"]
-    assert .00248 == values1.loc[50]["vals"]
+    assert 0.00029 == values1.loc[0]["vals"]
+    assert 0.00248 == values1.loc[50]["vals"]
